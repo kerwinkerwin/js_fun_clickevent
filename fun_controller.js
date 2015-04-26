6b6e181f787e6afd9f5cb063cefcb9d4310cc4a1
kerwinkerwin <kerwin@enspiral.com>
2015-04-26 18:42:46 +1200

    click working on mouse coord's

--- fun_controller.js
+++ fun_controller.js
@@ -1,21 +1,31 @@
 $(function(){
   var cat = new Cat
-  $('html').click(function(e){
-    cat.add($(e.target),e.screenX,e.screenY);
+  $('div').click(function(e){
+
+    console.log(e.pageX);
+    console.log(e.pageY);
+    var mouseX = event.clientX + document.body.scrollLeft;
+    var mouseY = event.clientY + document.body.scrollTop;
+    console.log(mouseX);
+    console.log(mouseY);
+    cat.add($(e.target),mouseX,mouseY);
   });
 });
 
 
 function Cat (){
   this.image_url = "http://www.us.onsior.com/images/hero-cat-home_01.png"
+  this.cat_count = 0;
 }
 
 Cat.prototype.add = function(elem,x,y){
   console.log(y);
-  var cat = ("<img id=meow src ="+this.image_url+"></img>");
+  console.log(x);
+  var cat = ("<img id="+this.cat_count+" src ="+this.image_url+"></img>");
   var meow = $(elem).append(cat);
-  $('#meow').css('top',""+y+'px');
-  $('#meow').css('left',""+x+'px');
+  $('#'+this.cat_count+"").css('top',""+y+'px');
+  $('#'+this.cat_count+"").css('left',""+x+'px');
+  this.cat_count ++;
 
 
 };
